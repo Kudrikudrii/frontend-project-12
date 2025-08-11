147228cab79295954cb83abedb35c979aebe9549
Sergei Garankin <gudri@bk.ru>
2025-08-11 13:15:55 +0300

    updated App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginForm from './components/LoginForm.jsx';
 import MainPage from './components/MainPage.jsx';
 import PageNotFound from './components/PageNotFound.jsx';
+import ChannelPage from './components/ChannelPage.jsx';
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<MainPage />} />
+        <Route index element={<ChannelPage />} />
         <Route path='login' element={<LoginForm />} />
         <Route path='*' element={<PageNotFound />} />
       </Routes>
