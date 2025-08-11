cc82029669e19c0981b21e914ec7d4ed1ccb8d46
Sergei Garankin <gudri@bk.ru>
2025-08-11 13:18:26 +0300

    fix paths App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginForm from './components/LoginForm.jsx';
-import MainPage from './components/MainPage.jsx';
+// import MainPage from './components/MainPage.jsx';
 import PageNotFound from './components/PageNotFound.jsx';
-import ChannelPage from './components/ChannelPage.jsx';
+import ChannelsPage from './components/ChannelsPage.jsx';
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<ChannelPage />} />
+        <Route index element={<ChannelsPage />} />
         <Route path='login' element={<LoginForm />} />
         <Route path='*' element={<PageNotFound />} />
       </Routes>
