148ba39af5fff267ebc42d17377e713c7a34ddf1
Sergei Garankin <gudri@bk.ru>
2025-08-16 14:47:00 +0300

    update frontend/src/pages/ChannelsPage.jsx

--- frontend/src/pages/ChannelsPage.jsx
+++ frontend/src/pages/ChannelsPage.jsx
@@ -1,27 +1,36 @@
 import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { setChannels } from '../slices/chatSlice.js';
-// import routes from '../routes.js';
+import routes from '../routes.js';
 import getAuthToken from '../getAuthToken.js';
 import axios from 'axios';
 import Channels from '../components/Channels.jsx';
+import Messages from '../components/Messages.jsx';
+import { setMessages } from '../slices/messagesSlice.js';
 
 const ChannelsPage = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const fullState = useSelector((state) => state);
-  console.log('Full Redux state:', fullState);
+  const [currentChannelId, setCurrentChannelId] = useState('');
+
   useEffect(() => {
     const fetchContent = async () => {
       try {
-        const response = await axios.get('/api/v1/channels', {
+        const responseChannels = await axios.get(routes.channelsPath(), {
           headers: getAuthToken(),
         });
-        const channels = response.data;
-        console.log('API Response:', response.data);
+        const channels = responseChannels.data;
+        console.log('API Response:', responseChannels.data);
         dispatch(setChannels(channels));
-        setCurrentChannel(channels[0].id);
+        setCurrentChannelId(channels[0].id);
+
+        const responseMessages = await axios.get(routes.messagesPath(), {
+          headers: getAuthToken(),
+        });
+        const messages = responseMessages.data;
+        console.log('API Response (messages):', responseMessages.data);
+        dispatch(setMessages(messages));
       } catch (err) {
         console.error('Ошибка при загрузке каналов:', err);
         if (err.response?.status === 401) {
@@ -42,10 +51,6 @@ const ChannelsPage = () => {
     localStorage.removeItem('token');
     navigate('/login');
   };
-  const channels = useSelector((state) => state.channels.channels);
-  console.log(channels);
-  const [currentChannel, setCurrentChannel] = useState('');
-  const activeChannelData = channels.find((channel) => channel.id === currentChannel);
 
   return (
     <div className='h-100 bg-light'>
@@ -62,41 +67,10 @@ const ChannelsPage = () => {
                 </button>
               </div>
             </nav>
-        
             <div className='container h-100 my-4 overflow-hidden rounded shadow'>
               <div className='row h-100 bg-white flex-md-row'>
-                <Channels currentChannel={currentChannel}  handleClick={setCurrentChannel} />
-                <div className='col p-0 h-100'>
-                  <div className='d-flex flex-column h-100'>
-                    {activeChannelData && (
-                      <div className='bg-light mb-4 p-3 shadow-sm small'>
-                        <p className='m-0'>
-                          <b># {activeChannelData.name}</b>
-                        </p>
-                        <span className='text-muted'>0 сообщений</span>
-                      </div>
-                    )}
-
-                    <div id='messages-box' className='chat-messages overflow-auto px-5'></div>
-
-                    <div className='mt-auto px-5 py-3'>
-                      <form noValidate className='py-1 border rounded-2'>
-                        <div className='input-group has-validation'>
-                          <input name='body' aria-label='Новое сообщение' placeholder='Введите сообщение...' className='border-0 p-0 ps-2 form-control' value='' />
-                          <button type='submit' disabled className='btn btn-group-vertical'>
-                            <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' width='20' height='20' fill='currentColor' className='bi bi-arrow-right-square'>
-                              <path
-                                fillRule='evenodd'
-                                d='M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z'
-                              />
-                            </svg>
-                            <span className='visually-hidden'>Отправить</span>
-                          </button>
-                        </div>
-                      </form>
-                    </div>
-                  </div>
-                </div>
+                <Channels currentChannelId={currentChannelId}  handleClick={setCurrentChannelId} />
+                <Messages currentChannel={currentChannelId} />
               </div>
             </div>
             <div className='Toastify'></div>
