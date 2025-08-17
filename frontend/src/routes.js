aebefd8bf44a1a5c7dfbb3ab42654bee4a42f90b
Sergei Garankin <gudri@bk.ru>
2025-08-18 01:07:30 +0300

    improve routes

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,7 +3,8 @@ const apiPath = '/api/v1'
 export default {
   loginPath: () => [apiPath, 'login'].join('/'),
   dataPath: () => [apiPath, 'data'].join('/'),
-  messagesPath: () => [apiPath, 'messages'].join('/'),
-  channelsPath: () => [apiPath, 'channels'].join('/'),
+  // messagesPath: () => [apiPath, 'messages'].join('/'),
+  messagesPath: (id = '') => [apiPath, 'messages', id].join('/').replace(/\/+$/, ''),
+  channelsPath: (id = '') => [apiPath, 'channels', id].join('/').replace(/\/+$/, ''),
   newUserPath: () => [apiPath, 'signup'].join('/'),
 }
