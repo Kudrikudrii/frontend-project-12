e7431a49cff43e9aef291129e0cf4ed6fb3dd516
Sergei Garankin <gudri@bk.ru>
2025-08-17 12:04:27 +0300

    h-100 > vh-100

--- frontend/src/pages/ChannelsPage.jsx
+++ frontend/src/pages/ChannelsPage.jsx
@@ -55,7 +55,7 @@ const ChannelsPage = () => {
   };
 
   return (
-    <div className='h-100 bg-light'>
+    <div className='vh-100 bg-light'>
       <div className='h-100'>
         <div className='h-100' id='chat'>
           <div className='d-flex flex-column h-100'>
