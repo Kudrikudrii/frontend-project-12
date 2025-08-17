f69d1c0e6f30e2ce2ad74b181520be477fdf8e10
Sergei Garankin <gudri@bk.ru>
2025-08-18 01:04:48 +0300

    update for 6 step

--- frontend/src/components/Channels.jsx
+++ frontend/src/components/Channels.jsx
@@ -1,13 +1,45 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useState } from 'react';
 import Channel from './Channel';
+import AddChannelModal from './modal/AddChannelModal';
+import { useEffect } from 'react';
+import { addChannel, removeChannel } from '../slices/channelsSlice';
+import { removeMessages } from '../slices/messagesSlice';
+import socket from '../socket';
 
-const Channels = ({ currentChannelId, handleClick }) =>  {
-    const channels = useSelector((state) => state.channels.channels)
+const Channels = ({ currentChannelId, handleClick, defaultChannelId }) =>  {
+  const dispatch = useDispatch();
+  const channels = useSelector((state) => state.channels.channels)
+  const [showModal, setShowModal] = useState(false);
+
+  useEffect(() => {
+    const handleNewChannel = (channel) => {
+      dispatch(addChannel(channel)); // { id: '3', name: 'new name channel', removable: true }
+      handleClick(channel.id);
+    };
+
+    const handleChannelRemoved = ({ id }) => {
+      dispatch(removeChannel({ id }));
+      dispatch(removeMessages({ channelId: id }));
+      if (currentChannelId === id) {
+        handleClick(defaultChannelId);
+      } 
+    };
+    
+    socket.on('newChannel', handleNewChannel);
+    socket.on('removeChannel', handleChannelRemoved);
+
+    return () => {
+      socket.off('newChannel', handleNewChannel);
+      socket.off('removeChannel', handleChannelRemoved);
+    } 
+  }, [currentChannelId, defaultChannelId, dispatch, handleClick]);
+    
     return (
     <div className='col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex'>
         <div className='d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-4'>
         <b>Каналы</b>
-        <button type='button' className='p-0 text-primary btn btn-group-vertical'>
+        <button type='button' className='p-0 text-primary btn btn-group-vertical' onClick={() => setShowModal(true)}>
             <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' width='20' height='20' fill='currentColor' className='bi bi-plus-square'>
             <path d='M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z'></path>
             <path d='M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4'></path>
@@ -15,15 +47,19 @@ const Channels = ({ currentChannelId, handleClick }) =>  {
             <span className='visually-hidden'>+</span>
         </button>
         </div>
-
+        <AddChannelModal 
+          show={showModal}
+          onClose={() => setShowModal(false)}
+        />
         <ul id='channels-box' className='nav flex-column nav-pills nav-fill px-2 mb-3 overflow-auto h-100 d-block'>
         {channels.map((channel) => (
         <Channel
-            key={channel.id}
-            channel={channel}
-            currentChannelId={currentChannelId}
-            handleClick={handleClick}
-            />
+          key={channel.id}
+          channel={channel}
+          currentChannelId={currentChannelId}
+          handleClick={handleClick}
+          defaultChannelId={defaultChannelId}
+        />
         ))}
         </ul>
     </div>
