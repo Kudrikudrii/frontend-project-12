b13e2c8dbc34f9e6263708449425f253febd4881
Sergei Garankin <gudri@bk.ru>
2025-08-18 01:02:22 +0300

    add modal components

--- frontend/src/components/Channel.jsx
+++ frontend/src/components/Channel.jsx
@@ -1,16 +1,65 @@
 import { Button } from 'react-bootstrap';
+import { useState } from 'react';
+import { Dropdown } from 'react-bootstrap';
+import RemoveChannelModal from './modal/RemoveChannelModal';
+import RenameChannelModal from './modal/RenameChannelModal';
 
-const Channel = ({ channel, handleClick, currentChannelId }) => {
+const Channel = ({ channel, handleClick, currentChannelId, defaultChannelId }) => {
+  const [showRenameModal, setShowRenameModal] = useState(false);
+  const [showRemoveModal, setShowRemoveModal] = useState(false);
+  const isActive = channel.id === currentChannelId;
+  const isRemovable = channel.removable ? true : false;
+
   return (
     <li className='nav-item w-100'>
-      <Button
-        variant={currentChannelId === channel.id ? 'secondary' : 'light'}
-        className='w-100 rounded-0 text-start btn'
-        onClick={() => handleClick(channel.id)}
-      >
-        <span className='me-1'>#</span>
-        {channel.name}
-      </Button>
+      <div className="d-flex dropdown btn-group">
+        <Button
+          variant={isActive ? 'secondary' : ''}
+          className='w-100 rounded-0 text-start btn'
+          onClick={() => handleClick(channel.id)}
+        >
+          <span className='me-1'>#</span>
+          {channel.name}
+        </Button>
+
+        {isRemovable && (
+            <Dropdown>
+              <Dropdown.Toggle
+                split
+                variant={isActive ? 'primary' : ''}
+                className="flex-grow-0 rounded-0 dropdown-toggle dropdown-toggle-split btn"
+              >
+                <span className="visually-hidden">Управление каналом</span>
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                <Dropdown.Item onClick={() => setShowRenameModal(true)}>
+                  Переименовать
+                </Dropdown.Item>
+                <Dropdown.Item onClick={() => setShowRemoveModal(true)}>
+                  Удалить
+                </Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          )}
+
+          <RenameChannelModal
+            show={showRenameModal}
+            onClose={() => setShowRenameModal(false)}
+            channelId={channel.id}
+            currentName={channel.name}
+          />
+
+          <RemoveChannelModal
+            show={showRemoveModal}
+            onClose={() => setShowRemoveModal(false)}
+            channelId={channel.id}
+            channelName={channel.name}
+            defaultChannelId={defaultChannelId}
+            currentChannelId={currentChannelId}
+            handleSwitchChannel={handleClick}
+          />
+      </div>      
     </li>
   );
 };
