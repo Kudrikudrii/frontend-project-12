60bdfdd94dad724a979d0e78cc438abf4b2c1d80
Сергей <Sergei@MacBook-Air-Sergej.local>
2025-09-25 22:13:04 +0300

    fix area label problem in signup form

--- frontend/src/components/MessageForm.jsx
+++ frontend/src/components/MessageForm.jsx
@@ -1,12 +1,12 @@
-import { useFormik } from 'formik';
-import getAuthToken from '../getAuthToken';
-import axios from 'axios';
-import { useRef, useEffect } from 'react';
-import routes from '../routes';
-import { useTranslation } from 'react-i18next';
-import leoProfanity from 'leo-profanity';
-import { useRollbar } from '@rollbar/react';
-import { toast } from 'react-toastify';
+import { useFormik } from "formik";
+import getAuthToken from "../getAuthToken";
+import axios from "axios";
+import { useRef, useEffect } from "react";
+import routes from "../routes";
+import { useTranslation } from "react-i18next";
+import leoProfanity from "leo-profanity";
+import { useRollbar } from "@rollbar/react";
+import { toast } from "react-toastify";
 
 const MessageForm = ({ currentChannelId, username }) => {
   const { t } = useTranslation();
@@ -19,7 +19,7 @@ const MessageForm = ({ currentChannelId, username }) => {
 
   const formik = useFormik({
     initialValues: {
-      body: '',
+      body: "",
     },
     onSubmit: async (values) => {
       const filteredMessage = leoProfanity.clean(values.body);
@@ -35,16 +35,16 @@ const MessageForm = ({ currentChannelId, username }) => {
         formik.resetForm();
         inputRef.current.focus();
       } catch (error) {
-        console.error('Ошибка при отправке сообщения:', error);
-        rollbar.error('Ошибка при отправке сообщения:', error, {
+        console.error("Ошибка при отправке сообщения:", error);
+        rollbar.error("Ошибка при отправке сообщения:", error, {
           endpoint: routes.messagesPath(),
-          method: 'POST',
+          method: "POST",
           timestamp: new Date().toISOString(),
-          component: 'MessageForm',
+          component: "MessageForm",
           action: formik,
         });
-        if (error.status === 'FETCH_ERROR') {
-          toast.error(t('toast.fetchError'));
+        if (error.status === "FETCH_ERROR") {
+          toast.error(t("toast.fetchError"));
         }
       }
     },
@@ -54,39 +54,42 @@ const MessageForm = ({ currentChannelId, username }) => {
     <form
       onSubmit={formik.handleSubmit}
       noValidate
-      className='py-1 border rounded-2'
-      aria-label={t('chat.messageForm.formLabel')}>
-      <div className='input-group has-validation'>
+      className="py-1 border rounded-2"
+      aria-label={t("chat.messageForm.formLabel")}
+    >
+      <div className="input-group has-validation">
         <input
-          id='body'
-          name='body'
-          type='text'
-          placeholder={t('chat.messageForm.placeholder')}
-          aria-label={t('chat.messageForm.placeholder')}
-          className='border-0 p-0 ps-2 form-control'
+          id="body"
+          name="body"
+          type="text"
+          placeholder={t("chat.messageForm.placeholder")}
+          aria-label={t("chat.messageForm.newMessage")}
+          className="border-0 p-0 ps-2 form-control"
           onChange={formik.handleChange}
           value={formik.values.body}
           ref={inputRef}
         />
         <button
-          type='submit'
+          type="submit"
           disabled={!formik.values.body}
-          className='btn btn-group-vertical'
-          aria-label={t('chat.messageForm.submit')}>
+          className="btn btn-group-vertical"
+          aria-label={t("chat.messageForm.submit")}
+        >
           <svg
-            xmlns='http://www.w3.org/2000/svg'
-            viewBox='0 0 16 16'
-            width='20'
-            height='20'
-            fill='currentColor'
-            className='bi bi-arrow-right-square'>
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            width="20"
+            height="20"
+            fill="currentColor"
+            className="bi bi-arrow-right-square"
+          >
             <path
-              fillRule='evenodd'
-              d='M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z'
+              fillRule="evenodd"
+              d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"
             />
           </svg>
-          <span className='visually-hidden'>
-            {t('chat.messageForm.submit')}
+          <span className="visually-hidden">
+            {t("chat.messageForm.submit")}
           </span>
         </button>
       </div>
